97b331180e12f501e49d73378d150e66fd10c27b
Michael Reinprecht <michael.reinprecht@student.htldornbirn.at>
2021-06-07 09:24:20 +0200

    Testing build/deploy

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Test3'
+      home: MyHomePage(title: 'Holztechnik Reinprecht'
       ),
     );
   }
